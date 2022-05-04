f71a6039a3c462d36f26d2c2c6647dee9ffda960
juan castañeda <juanemporis123>
2022-05-04 15:30:41 -0500

    Initial Commit

---
+++ FabricaDeAutomoviles/Automovil.swift
@@ -0,0 +1,8 @@
+//
+//  Automovil.swift
+//  FabricaDeAutomoviles
+//
+//  Created by wendy manrique on 4/05/22.
+//
+
+import Foundation
---
+++ FabricaDeAutomoviles/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FabricaDeAutomoviles
+//
+//  Created by wendy manrique on 4/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
